738beb8deb920aed39e04c77f67fae9cde8ceddc
Abdelkabir El Majdoub <ael-majd@c2r7p9.1337.ma>
2026-02-11 12:56:31 +0100

    Final before push on intra

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -35,21 +35,33 @@ void	PmergeMe::checkArg(const std::string &str)
 	}
 }
 
-void PmergeMe::print()
-{
-	std::vector<int>::iterator it = v.begin();
-	std::cout << "Before: ";
-	for (; it != v.end(); it++)
-	{
-		std::cout << *it << " " ;
-	}
-	std::cout << std::endl;
-}
 void	PmergeMe::proceesArg(char **av, int ac)
 {
 	for (int i = 1; i < ac; i++)
 		checkArg(av[i]);
-	print();
+	std::cout << "Before: ";
+	for (size_t i = 0; i < v.size(); i++)
+		std::cout << v[i] << " ";
+	std::cout << std::endl;
+	clock_t start = clock();
+	fordJhonson(v);
+	clock_t end = clock();
+	double v_time = (double)(end - start) / CLOCKS_PER_SEC * 1e6;
+	
+	clock_t Dstart = clock();
+	fordJhonson(d);
+	clock_t Dend = clock();
+	double d_time = (double)Dend - Dstart / CLOCKS_PER_SEC * 1e6;
+	
+	std::cout << "after: ";
+	for (size_t i = 0; i < v.size(); i++)
+		std::cout << v[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "Time to process a range of " << v.size() 
+		<< " elements with std::vector : " << v_time << " us" << std::endl;
+	std::cout << "Time to process a range of " << d.size() 
+		<< " elements with std::deque : " << d_time <<  " us" << std::endl;
 }
 
 
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,6 +6,8 @@
 #include <exception>
 #include <sstream>
 #include <limits>
+#include <algorithm>
+#include <utility>
 
 
 class PmergeMe
@@ -22,3 +24,45 @@ class PmergeMe
 		void	proceesArg(char **av, int ac);
 		void	checkArg(const std::string &str);
 };
+
+template <typename T>
+void fordJhonson(T &c)
+{
+	if (c.size() <= 1)
+		return ;
+	std::vector<std::pair<int, int> > pairs;
+	int pendding = 0;
+	bool is_pending = (c.size() % 2 != 0);
+	if (is_pending)
+		pendding = c.back();
+
+	for (size_t i = 0; i + 1 < c.size(); i += 2)
+	{
+		int a = c[i];
+		int b = c[i + 1];
+		if (a > b) 
+			std::swap(a, b);
+		pairs.push_back(std::make_pair(a, b));
+	}
+	T big;
+	T small;
+
+	for (size_t i = 0; i < pairs.size(); i++)
+	{
+		small.push_back(pairs[i].first);
+		big.push_back(pairs[i].second);
+	}
+	fordJhonson(big);
+
+	for (size_t i = 0; i < small.size(); i++)
+	{
+		typename T::iterator pos = std::lower_bound(big.begin(), big.end(), small[i]);
+		big.insert(pos, small[i]);
+	}
+	if(is_pending)
+	{
+		typename T::iterator pos = std::lower_bound(big.begin(), big.end(), pendding);
+		big.insert(pos, pendding);
+	}
+	c = big;
+}
